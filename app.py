b303f1437bfd8f5d8b8381f1a61e8c58179b7333
T125079 <t125079@ed.sus.ac.jp>
2026-01-31 15:26:09 +0900

    sleeptime chart

--- app.py
+++ app.py
@@ -10,31 +10,33 @@ with st.sidebar:
     st.header("表示グラフオプション")
     graph_selection = st.radio(
         "表示するグラフの種類を選択してください。",
-        ["性別、年代ごとの棒グラフ", "性別、年代ごとの棒グラフ"]
+        ["性別、年代ごとの棒グラフ", "性別、年代ごとの円グラフ","睡眠時間別年代および性別の棒グラフ"]
     )
     
     if graph_selection == "性別、年代ごとの棒グラフ":
         graph_mode = 'bar'
-    else:
+    elif graph_selection == "性別、年代ごとの円グラフ":
         graph_mode = 'pie'
+    else:
+        graph_mode = 'bar2'
 
     st.header("フィルターオプション")
-    sex_options = st.multiselect(
-        "性別を選択してください。",
-        df['sex'].unique(),
-        default=df['sex'].unique()
-    )
+    table = st.toggle("表を表示する",value=True)
+    if graph_mode == 'bar' or graph_mode=='pie':
+        with st.expander("表示項目オプション"):
+            sex_options = st.multiselect(
+                "性別を選択してください。",
+                df['sex'].unique()
+            )
 
-    age_options = st.multiselect(
-        "年代を選択してください。",
-        df['age'].unique(),
-        default=df['age'].unique()
-    )
-
-filtered_df = df[
-    df['sex'].isin(sex_options) & 
-    df['age'].isin(age_options)
-].copy()
+            age_options = st.multiselect(
+                "年代を選択してください。",
+                df['age'].unique()
+            )
+        filtered_df = df[
+            df['sex'].isin(sex_options) & 
+            df['age'].isin(age_options)
+        ]
 
 if not filtered_df.empty:
     filtered_df['display_label'] = filtered_df['sex'] + " " + filtered_df['age']
@@ -47,8 +49,9 @@ if not filtered_df.empty:
         "display_label": "性別・年代"
     }
 
-    st.subheader("絞り込みデータ一覧")
-    st.dataframe(filtered_df.rename(columns=jp_columns), use_container_width=True)
+    if table:
+        st.subheader("絞り込みデータ一覧")
+        st.dataframe(filtered_df.rename(columns=jp_columns), use_container_width=True)
 
     if graph_mode == "bar":
         st.subheader("睡眠時間の構成比（性別・年代）棒グラフ")
@@ -76,9 +79,13 @@ if not filtered_df.empty:
                         values="percentage",
                         names="sleep",
                         hole=0.5,
-                        title=f"{sex} {age} の睡眠時間割合"
+                        title=f"睡眠時間割合"
                     )
+                    fig.update_layout(annotations=[{ "text": f"{sex}<br>{age}","x": 0.5, "y": 0.5,"font_size": 18,"showarrow": False }])
                     fig.update_traces(textinfo='percent+label')
                     st.plotly_chart(fig, use_container_width=True)
 else:
     st.warning("選択された条件に該当するデータがありません。")
+
+st.write('国民健康・栄養調査 / 令和５年国民健康・栄養調査 第71表　１日の平均睡眠時間 - １日の平均睡眠時間、年齢階級別、人数、割合 - 総数・男性・女性、20歳以上 を改変し作成')
+
