26f63b2d2cae3aa0904f0f026aba4dfebbb124ed
T125079 <t125079@ed.sus.ac.jp>
2026-01-31 13:04:27 +0900

    ver1.1.0

--- app.py
+++ app.py
@@ -4,57 +4,42 @@ import plotly.express as px
 st.title('１日の平均睡眠時間')
 df = pd.read_csv('sleep.csv')
 with st.sidebar:
+    st.sidebar.header("表示グラフオプション")
+    show_graph_options=st.radio(
+        "表示するグラフの種類を選択してください。",
+        ["性別、年代ごとの棒グラフ","性別ごとの円グラフ"]
+    )
+
     st.sidebar.header("フィルターオプション")
     st.write('フィルターするオプションを選択してください。')
     sex_options = st.multiselect(
         "性別を選択してください。（複数選択可）",
-        ["男性", "女性", "すべて"]
+        df['sex'].unique()
     )
-    sex_mapping = {
-        "男性": "male",
-        "女性": "female",
-        "すべて": "all"
-    }
-    selected_sex = [sex_mapping[item] for item in sex_options]
 
     age_options = st.multiselect(
         "年代を選択してください。（複数選択可）",
-        ["20代","30代","40代","50代","60代","70代以上","すべて"]
+        df['age'].unique()
     )
-    age_mapping = {
-        "20代": "20-29",
-        "30代": "30-39",
-        "40代": "40-49",
-        "50代": "50-59",
-        "60代": "60-69",
-        "70代以上": "70-",
-        "すべて": "all"
-    }
-    selected_age = [age_mapping[item] for item in age_options]
 
 filtered_df = df[
-    (df['sex'].isin(selected_sex)) & 
-    (df['age'].isin(selected_age))
+    df['sex'].isin(sex_options) &
+    df['age'].isin(age_options)
 ]
 
+
 if not filtered_df.empty:
-    inv_sex_map = {v: k for k, v in sex_mapping.items()}
-    inv_age_map = {v: k for k, v in age_mapping.items()}
-    sleep_mapping = {
-    "-5": "5時間未満",
-    "5-6": "5時間以上6時間未満",
-    "6-7": "6時間以上7時間未満",
-    "7-8": "7時間以上8時間未満",
-    "8-9": "8時間以上9時間未満",
-    "9-": "9時間以上"
-    }
-    filtered_df['sex'] = filtered_df['sex'].map(inv_sex_map)
-    filtered_df['age'] = filtered_df['age'].map(inv_age_map)
-    filtered_df['sleep'] = filtered_df['sleep'].map(sleep_mapping)
     filtered_df['display_label'] = filtered_df['sex'] + " " + filtered_df['age']
+    jp_columns = {
+    "sex": "性別",
+    "age": "年代",
+    "sleep": "睡眠時間",
+    "percentage": "割合（％）",
+    "display_label": "性別・年代"
+    }
 
     st.subheader("絞り込みデータ一覧")
-    st.dataframe(filtered_df, use_container_width=True)
+    st.dataframe(filtered_df.rename(columns=jp_columns),use_container_width=True)
     
     st.subheader("睡眠時間の構成比")
 
@@ -69,9 +54,7 @@ if not filtered_df.empty:
             "sleep": "睡眠時間"
         },
 
-        category_orders={
-            "display_label": sorted(filtered_df['display_label'].unique())
-        }
+       
     )
     st.plotly_chart(fig, use_container_width=True)
 
